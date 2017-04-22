35b7a5394e24b4b7d4271f79d7fd6374065ad576
ykim253 <ykim253@uw.edu>
2017-04-22 14:30:04 -0700

    adding Item drop chance stuff still working on it

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour {
+
+
+    [System.Serializable]
+    public class Items
+    {
+        public string name;
+        public GameObject item;
+        public int dropChance;
+    }
+
+    public List<Items> DropList = new List<Items>();
+    public bool isCrateDestroyed = false;
+
+    void dropLoot()
+    {
+        if(isCrateDestroyed)
+        {
+            int itemChance = 0;
+
+            for (int i = 0; i < DropList.Count; i++)
+            {
+                itemChance += DropList[i].dropChance;
+            }
+
+            int chosenOne = Random.Range(0, itemChance);
+
+            for (int j = 0; j < DropList.Count; j++)
+            {
+                if (chosenOne <= DropList[j].dropChance)
+                {
+                    Instantiate(DropList[j].item, transform.position, Quaternion.identity);
+                    return;
+                }
+                chosenOne -= DropList[j].dropChance;
+            }
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

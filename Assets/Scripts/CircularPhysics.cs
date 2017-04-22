e60c99722e4d61bf979facc885380ce26b5ae633
Hunter MacDonald <mactinite77@gmail.com>
2017-04-22 01:51:11 -0700

    added some initial physics code and changed scene files to be in text format

---
+++ Assets/Scripts/CircularPhysics.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularPhysics : MonoBehaviour {
+
+
+    public float gravitySpeed;
+
+    public List<Rigidbody2D> physicsObjects = new List<Rigidbody2D>();
+
+
+
+    private void Update()
+    {
+        Physics();
+    }
+
+    void Physics()
+    {
+        foreach(Rigidbody2D rb in physicsObjects)
+        {
+            rb.velocity = transform.position - rb.transform.position * gravitySpeed * Time.deltaTime;
+        }
+    }
+
+    public void RegisterRigidbody(Rigidbody2D rigidBody)
+    {
+        physicsObjects.Add(rigidBody);
+    }
+
+}

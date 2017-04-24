a424d93c220029bdf94ddc33cdd31b9ac7087f24
ykim253 <ykim253@uw.edu>
2017-04-23 22:28:25 -0700

    grab bomb by running into one and drop bomb

---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour {
+
+    public Transform holdAbove;
+    private GameObject pickedUp;
+    public bool grabbed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(pickedUp == null)
+        {
+            if (collision.gameObject.tag == "Item")
+            {
+                grabbed = true;
+                collision.gameObject.transform.position = holdAbove.position;
+                pickedUp = collision.gameObject;
+                pickedUp.GetComponent<Bomb>().grabbed = true;
+            }
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            if (pickedUp != null)
+            {
+                if (pickedUp.tag == "Item")
+                {
+                    grabbed = false;
+                    pickedUp.GetComponent<Bomb>().grabbed = false;
+                    pickedUp = null;
+                    
+                }
+                else
+                {
+                    print("not a bomb");
+                }
+            }
+        }
+		if (grabbed)
+        {
+
+            pickedUp.transform.position = holdAbove.position;
+            
+        }
+	}
+}
